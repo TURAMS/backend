a6805c25d8ecb371da9c8c9c81921fcb926d0a20
Ileecea Askew <72593006+IleeceaA@users.noreply.github.com>
2022-10-09 12:19:29 -0500

    backend api created

---
+++ main.py
@@ -0,0 +1,9 @@
+placesInAirzport = {
+    "TSA": [{"name": "Gate A", "lane": 1, "estTime": 25},{"name": "Gate B", "lane": 2, "time": 34}],
+    #call Mastercard API to get traffic and frequency to estimate a wait time
+    "Restaurant":[{"type": "coffee", "name": "Starbucks", "location": "A4", "time": "45"},
+                {"type": "coffee", "name": "Dunkin Donuts", "location": "A7", "time": 3}],
+    "Bathroom":[{"name":"Bathroom 1", "estTime": 10, "location": "A"},{"name": "Bathroom 2", "estTime": 15, 
+                                                                                                "location": "B"}]
+        
+}
